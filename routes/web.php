b27fbea8385c5f7dffb9056f33d69a64bb685859
Aulia153 <auliasilmi133@gmail.com>
2025-02-10 11:23:27 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+//ACARA 3
+//route name
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Basic route
+Route::get('/foo', function() {
+    return "Hello World";
+});
+
+//parameter route
+Route::get('/foo/{id}', function ($id) {
+    return 'User ' . $id;
 });
