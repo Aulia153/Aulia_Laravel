eb436217159b3cd0016f0b7bdbaa623f2e19e384
Aulia153 <auliasilmi133@gmail.com>
2025-02-22 22:26:33 +0700

    migration database

--- routes/web.php
+++ routes/web.php
@@ -172,5 +172,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Frontend'], function () {
 
 //Acara 8 backend
 Route::group(['namespace' => 'App\Http\Controllers\Backend'], function() {
-    Route::resource('dashboard', DashboardController::class);
+    Route::resource('dashboard', 'DashboardController');
 });
+
+//Acara 9
