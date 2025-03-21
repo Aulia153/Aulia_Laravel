16bab20c1bb4eaac6b39f7b92fa5bd24bfb5443b
Aulia153 <auliasilmi133@gmail.com>
2025-03-21 11:32:32 +0700

    revisi done

--- app/Models/Pendidikan.php
+++ app/Models/Pendidikan.php
@@ -20,13 +20,13 @@ class Pendidikan extends Model
     public function getTingkatanAttribute($value)
     {
         $levels = [
-            1 => 'SD',
-            2 => 'SMP',
-            3 => 'SMA',
-            4 => 'D3',
-            5 => 'S1',
-            6 => 'S2',
-            7 => 'S3'
+            1 => 'TK',
+            2 => 'SD',
+            3 => 'SMP',
+            4 => 'SMA/SMK',
+            5 => 'D3',
+            6 => 'D4/S1',
+            7 => 'S2'
         ];
 
         return $levels[$value] ?? 'Tidak Diketahui';
